5ce2e1f0004dfeb011b1c5d8685a0adecaddd578
Lucas Adriano de Souza <104021672+LucasAdrian0@users.noreply.github.com>
2026-09-14 20:01:04 -0300

    Configurando arquivo .env para conexão com o banco de dados do supabase

--- tcc_aplicativo_de_frete_e_mudancas/lib/main.dart
+++ tcc_aplicativo_de_frete_e_mudancas/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'presentation/app.dart';
 import 'package:flutter/material.dart';
 import 'core/supabase_config.dart';
@@ -6,6 +7,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Carrega as variáveis do arquivo .env da raiz do projeto
+  await dotenv.load(fileName: "dotenv.env");
+  // Inicializa o Supabase com validação da URI
   await SupabaseConfig.inicializar();
   runApp(MyApp(useCases: AppRepositories(Supabase.instance.client).useCases));
 }
